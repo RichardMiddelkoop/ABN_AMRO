74f8c2fc4b5bd73b803fd414005825e5122fbd6a
Richard <richard-drahcir@hotmail.com>
2024-02-27 11:56:18 +0100

    created select columns from df test

--- test_exercise.py
+++ test_exercise.py
@@ -24,3 +24,19 @@ def test_filter_column_by_string():
     ]
     expected_df = spark.createDataFrame(expected_data, ["country"])
     assert_df_equality(actual_df, expected_df)
+
+def test_select_columns_from_df():
+    spark = (SparkSession.builder
+            .master("local")
+            .appName("chispa")
+            .getOrCreate())
+    source_data = [
+        ("ABN", " ", "AMRO")
+    ]
+    source_df = spark.createDataFrame(source_data, ["1","2","3"])
+    actual_df = select_columns_from_df(source_df, "1", "3")
+    expected_data = [
+        ("ABN", "AMRO")
+    ]
+    expected_df = spark.createDataFrame(expected_data, ["1", "3"])
+    assert_df_equality(actual_df, expected_df)
