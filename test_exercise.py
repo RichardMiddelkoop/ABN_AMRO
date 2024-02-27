8347f0d56fa1ed61b4740418debc19d5aa1e2936
Richard <richard-drahcir@hotmail.com>
2024-02-27 09:02:34 +0100

    created initial chispa test

--- test_exercise.py
+++ test_exercise.py
@@ -1,6 +1,30 @@
+import pytest
+from chispa.dataframe_comparer import *
+import pyspark.sql.functions as F
+from exercise import *
+from pyspark.sql import SparkSession
 
-def initial_function(x):
-    return x.upper()
 
-def test_initial_function():
-    assert initial_function('abn_amro') == 'ABN_AMRO'
+def test_filter_column_by_string():
+    spark = (SparkSession.builder
+            .master("local")
+            .appName("chispa")
+            .getOrCreate())
+    source_data = [
+        ("United Kingdom",),
+        ("UnitedKingdom",),
+        ("Netherlands",),
+        ("!#@__UK&&"),
+        (None,)
+    ]
+    source_df = spark.createDataFrame(source_data, ["country"])
+    actual_df = source_df.withColumn(
+        "clean_country",
+        filter_column_by_string(source_df, F.col("country"), "Netherlands, United Kingdom")
+    )
+    expected_data = [
+        ("United Kingdom",),
+        ("Netherlands",)
+    ]
+    expected_df = spark.createDataFrame(expected_data, ["country", "clean_country"])
+    assert_df_equality(actual_df, expected_df)
